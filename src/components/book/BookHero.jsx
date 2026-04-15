a21d604d155beb4ad71e816fe3635aff5d2eb39d
tusharjain2001 <tusharjainqwerty@gmail.com>
2026-04-15 17:47:34 +0530

    fixed book hero

--- src/components/book/BookHero.jsx
+++ src/components/book/BookHero.jsx
@@ -1,60 +1,56 @@
-import heroImg from '../../assets/images/book/featured-book-hero.png';
-import heroEllipse from '../../assets/images/book/hero-ellipse.svg';
-import starRating from '../../assets/images/book/star-rating.svg';
-import arrowRight from '../../assets/images/arrow-right.svg';
+import heroComposite from "../../assets/images/book-hero.svg";
+import starRating from "../../assets/images/book/star-rating.svg";
+import arrowRight from "../../assets/images/arrow-right.svg";
 
 export default function BookHero() {
   return (
-    <section className="bg-[#e4e8d7] min-h-[823px] relative overflow-hidden flex items-center">
-      <div className="max-w-[1300px] mx-auto px-8 py-20 flex items-center gap-16 w-full">
-        {/* Left: book cover with decorative ellipse */}
-        <div className="relative flex-shrink-0 w-[336px]">
-          {/* Decorative circle behind cover */}
+    <section className="bg-[#e4e8d7]  relative overflow-hidden flex items-center">
+      <div className=" px-20 py-20 grid grid-cols-[430px_minmax(0,1fr)] items-center gap-20 w-full">
+        {/* Left: combined book hero artwork */}
+        <div className="relative justify-self-center w-[475px]">
           <img
-            src={heroEllipse}
-            alt=""
-            className="absolute -left-[69px] -top-[15px] w-[475px] h-[475px] pointer-events-none"
+            src={heroComposite}
+            alt="The Fragrance Shed by a Violet"
+            className="w-[475px] h-auto block"
           />
-          <div className="relative shadow-[5px_5px_4px_0px_rgba(0,0,0,0.25)] w-[336px] h-[476px] overflow-hidden">
-            <img
-              src={heroImg}
-              alt="The Fragrance Shed by a Violet"
-              className="w-full h-full object-cover"
-            />
-          </div>
         </div>
 
         {/* Right: book info */}
-        <div className="flex flex-col gap-8 flex-1 max-w-[770px]">
+        <div className="flex flex-col gap-8 max-w-[1080px]">
           <div className="flex flex-col gap-[18px]">
-            <h1 className="font-['Aboreto'] text-[#111] text-[40px] leading-[53px]">
+            <h1 className="font-['Aboreto'] text-[#111] text-[40px] leading-[1.1] uppercase tracking-wide">
               The Fragrance Shed by a Violet
             </h1>
             <div className="flex items-center gap-2">
-              <img src={starRating} alt="4.7 stars" className="h-[17px]" />
-              <span className="font-['Questrial'] text-[#b83431] text-[19px]">4.7/5</span>
+              <img src={starRating} alt="4.7 stars" className="h-4" />
+              <span className="font-['Questrial'] text-[#b83431] text-[19px]">
+                4.7/5
+              </span>
             </div>
           </div>
 
-          <div className="font-['Questrial'] text-[#595959] text-base leading-[26px] text-justify space-y-4">
+          <div className="font-['Questrial'] text-[#595959] text-[16px] leading-[2] space-y-5 max-w-[1120px]">
             <p>
-              After her series of articles is awarded three Pulitzer Prizes, investigative reporter
-              Kate Townsend is haunted by the knowledge that her new-found fame has been purchased
-              at too high a price. She is certain that McCall, an internationally acclaimed
-              cardiologist, researcher, and a 2002 nominee for the Nobel Prize for Medicine, is not
-              guilty.
+              After her series of articles is awarded three Pulitzer Prizes,
+              investigative reporter Kate Townsend is haunted by the knowledge
+              that her new-found fame has been purchased at too high a price.
+              She is certain that McCall, an internationally acclaimed
+              cardiologist, researcher, and a 2002 nominee for the Nobel Prize
+              for Medicine, is not guilty.
             </p>
             <p>
-              Texas governor Greg Bell hires former homicide detective and criminal defense attorney
-              Rich Jansen to fix the escalating problems at the Huntsville Prisons recently inflamed
-              by a lawsuit against infamous inmate Dr. Lindsey McCall. When Jansen's skills quickly
-              result in the resignation of an incompetent prison medical director, he realizes that
-              this strange saga is just beginning.
+              Texas governor Greg Bell hires former homicide detective and
+              criminal defense attorney Rich Jansen to fix the escalating
+              problems at the Huntsville Prisons recently inflamed by a lawsuit
+              against infamous inmate Dr. Lindsey McCall. When Jansen's skills
+              quickly result in the resignation of an incompetent prison medical
+              director, he realizes that this strange saga is just beginning.
             </p>
             <p>
-              Mark Twain wrote that forgiveness was the fragrance shed by a violet upon the heel of
-              the boot that has crushed it. This medical mystery weaves together the lives of two
-              sisters, Lindsey and Paula, with those of strangers as each copes with loss, betrayal,
+              Mark Twain wrote that forgiveness was the fragrance shed by a
+              violet upon the heel of the boot that has crushed it. This medical
+              mystery weaves together the lives of two sisters, Lindsey and
+              Paula, with those of strangers as each copes with loss, betrayal,
               jealousy, and the exquisitely painful journey to forgiveness.
             </p>
           </div>
@@ -63,7 +59,11 @@ export default function BookHero() {
             <span className="font-['Questrial'] text-white text-base uppercase whitespace-nowrap">
               EXPLORE MORE BOOKS from the author
             </span>
-            <img src={arrowRight} alt="" className="w-6 h-6 rotate-90 brightness-0 invert" />
+            <img
+              src={arrowRight}
+              alt=""
+              className="w-6 h-6 rotate-90 brightness-0 invert"
+            />
           </button>
         </div>
       </div>
